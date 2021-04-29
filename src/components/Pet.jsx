3d43d5ffcaf9e2a05b6cf00c4e585b2f1147fac8
borsikapiros <73432795+borsikapiros@users.noreply.github.com>
2021-04-29 22:26:48 +0200

    post

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -1,34 +1,37 @@
 import { useState } from "react";
 
 export default function Pet(props) {
-  const pet = props.pet;
-  const vacc = pet.isVaccinated;
-  const [vaccText, setVaccText] = useState(vacc);
-  console.log(vaccText);
+    const pet = props.pet;
+    const vacc = pet.isVaccinated;
+    const [vaccText, setVaccText] = useState(vacc);
+    const [resp, setResp] = useState(true);
+    console.log(vaccText);
 
-  function toggle() {
-    fetch("api/pets/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name: pet.name, isVaccinated: vacc }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data) {
-          vaccText === true ? setVaccText(false) : setVaccText(true);
-        } else {
-          setVaccText("...");
-        }
-      });
-  }
+    function toggle() {
+        fetch("api/pets/", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name: pet.name, isVaccinated: vacc }),
+        })
+            .then((response) => setResp(true))
+            .then((data) => {
+                if (data) {
+                    vaccText === true ? setVaccText(false) : setVaccText(true);
+                } else {
+                    setVaccText("...");
+                }
+            });
+    }
 
-  const showText = vaccText === false ? "false" : "true";
+    const showText = vaccText === false ? "false" : "true";
 
-  return (
-    <div>
-      {pet.name} - Vaccinated: <button onClick={toggle}>{showText}</button>
-    </div>
-  );
+    return (
+        <div>
+            {pet.name} - Vaccinated:
+            <button onClick={toggle}>{showText}</button>
+        </div>
+    );
 }
